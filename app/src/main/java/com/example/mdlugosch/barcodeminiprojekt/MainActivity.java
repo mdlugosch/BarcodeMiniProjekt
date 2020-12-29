d2142a5ca53746fd4d7a9a07ffdae32cfc39a93e
Michael Dlugosch <michael.dlugosch@web.de>
2020-12-29 21:57:55 +0100

    Barcode und QR-Code Scan App

--- app/src/main/java/com/example/mdlugosch/barcodeminiprojekt/MainActivity.java
+++ app/src/main/java/com/example/mdlugosch/barcodeminiprojekt/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.mdlugosch.barcodeminiprojekt;
 
 import android.Manifest;
-import android.app.ActionBar;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -19,9 +15,8 @@ import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
-    // Definieren der Objektvariablen die später mit den UI-Elementen in Beziehung gesetzt werden
+    // Definieren der Objektvariablen, die später mit den UI-Elementen in Beziehung gesetzt werden
     private TextView barcodeTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +25,12 @@ public class MainActivity extends AppCompatActivity {
         // Objektvariable auf UI-Element Referenzieren lassen
         barcodeTextView = findViewById(R.id.barcodetextview);
 
-        // Erlaubnis vom Benutzer zur nutzung der Kamera einhiolen.
+        // Erlaubnis vom Benutzer zur Nutzung der Kamera einholen.
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
     }
 
     public void ScanButton(View view) {
-        // Scan Objekt erstellen das auf die Scanbibliothek zugreift.
+        // Scan Objekt erstellen, dass auf die Scanbibliothek zugreift.
         IntentIntegrator intentIntegrator = new IntentIntegrator(this);
         intentIntegrator.initiateScan();
     }
@@ -45,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         // Auslesen des Ereignisses
         IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
 
-        // Liefert das Ereigniss Daten?
+        // Liefert das Ereignis Daten?
         if(intentResult != null) {
             // Hat der ausgelesene Code Inhalt?
             if(intentResult.getContents() == null) {
